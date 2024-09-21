702d7d1fe841d379c86a3165f71d3dd3e8a26e43
ronithussain <xayaanhussain542@gmail.com>
2024-09-21 21:07:32 +0600

    added validation and file

--- js/addmoney2.js
+++ js/addmoney2.js
@@ -5,15 +5,27 @@ document.getElementById('btn-add-money')
         const addMoney = gitInputFieldValueById('input-add-money');
         const pinNumber = gitInputFieldValueById('input-pin-number');
         
+        if(isNaN(addMoney)){
+            alert('Failed to add money')
+            return;
+        }
+
 
         if(pinNumber === 1234){
 
-            const balance = gitTextFieldValueById('account-balance');
-            console.log(balance,addMoney);
-            const newBalance = balance + addMoney;
-            console.log(newBalance);
-            document.getElementById('account-balance').innerText = newBalance;
-          
+          const balance = gitTextFieldValueById('account-balance');
+          // console.log(balance,addMoney);
+          const newBalance = balance + addMoney;
+          // console.log(newBalance);
+          document.getElementById('account-balance').innerText = newBalance;
+
+          // add to transaction history   
+          const p = document.createElement('p');
+          p.innerText = `Added: ${addMoney} Tk. New Balance: ${ newBalance}`;
+          console.log(p);
+
+          // should be a common function
+          document.getElementById('transaction-container').appendChild(p);
          
         }
         else{
--- js/cashout.js
+++ js/cashout.js
@@ -7,13 +7,34 @@ document.getElementById('btn-cash-out')
         const pinNumber = gitInputFieldValueById('input-cash-out-pin');
         
 
-       if(pinNumber === 1234){
+        if(isNaN(cashOut)){
+            alert('Failed to cash out')
+            return;
+        }
 
+       if(pinNumber === 1234){
         const balance = gitTextFieldValueById('account-balance');
+
+        if(cashOut > balance){
+            alert('You do not have enough money to cash out');
+            return;
+        }
+
         const newBalance = balance - cashOut;
         // console.log(newBalance);
         document.getElementById('account-balance').innerText = newBalance;
 
+        // add to transaction history
+        const div = document.createElement('div');
+        div.classList.add('bg-yellow-300')
+        div.classList.add('p-3');
+        div.classList.add('rounded-xl');
+        div.classList.add('mt-3');
+        div.innerHTML= `
+        <h4 class="text-2xl font-bold"></h4>
+        <p> ${cashOut} withdraw. New Balance ${newBalance} </p>
+        `
+        document.getElementById('transaction-container').appendChild(div);
 
        }
        else{
